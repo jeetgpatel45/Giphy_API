1a8e3a1bbec74d8ee3346a8e77302d9821351b1f
jeetgpatel45 <jeetgpatel45@yahoo.com>
2019-07-04 22:52:31 -0400

    took some while to finish

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Giphy_API
+# Giphy_API Assignment
+
+# Took lots of help from the activities we did in class.
+
+1. For this assignment, I am creating a webpage using an API's for the GIFS. 
+
+2. Instruction that are Given to me:
+    1. Create an Array of the things I like, then make that array a buttons.
+    2. When the user clicks on the button, the page should get the gif images which are still.
+    3. When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+    4. Under the gif's images, display a rating of that gif.
+    5. Lastly, make a search bar, so the user can put the input that adds it to an array to create a button.
+
+3. Deploy all wepage to Github.
+
+# With this assignment I am getting to learn Javascript better.
+# Tried my Creativity.
--- giphy.js
+++ giphy.js
@@ -1,56 +1,78 @@
 $(document).ready(function () {
-    var marvelGiphy = ["Spiderman", "Deadpool", "X-Men", "Doctor Strange", "Avengers", "Captain America", "Iron Man", "Thor", "The Incredible Hulk"]
-    $(".btn-primary").on("click", function button() {
-    $(".btn-primary").remove();
 
-    // CREATING BUTTONS OF AN ARRAY USING LOOPS
-    for (var i = 0; i < marvelGiphy.length; i++) {
-        var a = $("<button>");
-        a.addClass("giphy-buttons");
-        a.attr("giphy-name", marvelGiphy[i]);
-        a.text(marvelGiphy[i]);
-        $("#start").append(a);
-    }
-})
+    var marvelGiphy = ["Spiderman", "Deadpool", "Wolverine", "Doctor Strange", "Avengers", "Captain America", "Iron Man", "Thor", "Hulk Smash"]
 
-    // GETTING THE API'S BY CLICKING THE BUTTON
-    $(".giphy-buttons").on("click", function giphy() {
-        $("#giphy-here").html("")
-        var giphy = $(this).attr("giphy-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            giphy + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
+    $(".btn-primary").on("click", function buttons() {
+        $(".btn-primary").remove();
 
-        $.ajax({
-            url: queryURL,
-            method: "GET",
-        }).then(function (response) {
-            var results = response.data;
+        // CREATING BUTTONS OF AN ARRAY USING LOOPS
+        for (var i = 0; i < marvelGiphy.length; i++) {
+            var Button = $("<button>");
+            Button.addClass("giphy-buttons");
+            Button.attr("giphy-name", marvelGiphy[i]);
+            Button.text(marvelGiphy[i]);
+            $("#start").append(Button);
+        }
+        
+        $(".giphy-buttons").on("click", function giphy() {
+            // console.log("you clicked")
 
-            for (var i = 0; i < results.length; i++) {
-                var gifDiv = $("<div>");
+            $("#giphy-here").html("")
+            var giphy = $(this).attr("giphy-name");
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+                giphy + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
 
-                var rating = results[i].rating;
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
+                //   console.log(response.data.length)
+                var results = response.data;
+                //   $("#giphy-here").prepend(queryURL)
+                for (var i = 0; i < results.length; i++) {
+                    var rating = results[i].rating;
+                    // console.log(rating)
 
-                var giphyRating = $("<p>").text("Rating: " + rating);
-                gifDiv.append(giphyRating);
+                    var giphyRating = $("<h4>").html("Rating: " + rating);
+                    // console.log(giphyRating)
 
-                var giphyImage = $("<img>");
-                giphyImage.attr("data-animate", results[i].images.fixed_height.url);
-                giphyImage.attr("data-still", results[i].images.fixed_height_still.url)
-                giphyImage.attr("src", results[i].images.fixed_height.url);
+                    var giphyImage = $("<img>");
+                    giphyImage.attr("data-animate", results[i].images.fixed_height.url);
+                    giphyImage.attr("data-still", results[i].images.fixed_height_still.url)
+                    giphyImage.attr("src", results[i].images.fixed_height.url);
+                    giphyImage.attr("data-state", "still")
+                    giphyImage.addClass("animate");
+                    // console.log(giphyImage)
 
-                gifDiv.prepend(p);
-                gifDiv.prepend(giphyImage);
+                    var gifDiv = $("<div>");
+                    gifDiv.prepend(giphyRating);
+                    gifDiv.prepend(giphyImage);
+                    // console.log(gifDiv)
 
-                $("#giphy-here").prepend(gifDiv);
-            }
-        });
+                    $("#giphy-here").prepend(gifDiv)
+
+                }
+            })
+        });    
+        $(".btn-outline-success").on("click", function (event) {
+            event.preventDefault();
+            var newGiphy = $(".form-control").val().trim();
+            // console.log(newGiphy)
+            marvelGiphy.push(newGiphy);
+            // console.log(marvelGiphy)
+            buttons()
+        })
     });
-
-    $("#search").on("click", function(event) {
-        event.preventDefault();
-        var newGiphy = $(".form-control").val().trim();
-        marvelGiphy.push(newGiphy);
-        button()
-    })
+    $(document).on("click", ".animate", function() {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      });
 })
+
+
